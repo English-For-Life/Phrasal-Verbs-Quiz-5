1c44bc0e61c93e3389410df35faa22dc727cdc73
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-02 15:03:53 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+//Array of objects
+const quiz = [
+    {
+        q:"He went ___ to the cellar to bring a bottle of wine.",
+        options:["down", "away", "in", "ahead"],
+        answer: 0
+    },
+    {
+     q:"Go ___ ! I can't talk to you right now I'm very busy.",
+        options:["off", "away", "down", "up"],
+        answer: 1   
+    },
+    {
+        q:"My parents have gone ___ for dinner. They'll be back in an hour.",
+        options:["after", "on", "out", "off"],
+        answer: 2
+    },
+    {
+        q:"Jack tried to go ___ the robber but he failed to catch him. ",
+        options:["over", "up", "out", "after"],
+        answer: 3
+    },
+    {
+        q:"New apartments are going ___ all over the west coast.",
+        options:["up", "off", "in", "for"],
+        answer: 0
+    },
+    {
+        q:"The bomb went ___ but fortunately no one was seriously injured.",
+        options:["on", "off", "out", "up"],
+        answer: 1
+    },
+    {
+        q:"Mr. Robertson doesn't go much ___ modern architecture.",
+        options:["on", "away", "for", "in"],
+        answer: 2
+    },
+    {
+        q:"Yoy should go ___ your essay before you hand it in to your teacher.",
+        options:["off", "up", "down", "over"],
+        answer: 3
+    },
+    {
+        q:"Let's go ___ to the hotel. I'm exhausted after five hours of sightseeing. ",
+        options:["back", "away", "over", "after"],
+        answer: 0
+    },
+    {
+        q:"I've been going ___ that job for three months.",
+        options:["up", "after", "on", "off"],
+        answer: 1
+    },
+    {
+        q:"My sister always goes ___ vivid colours of her outfits.",
+        options:["away", "out", "for", "over"],
+        answer: 2
+    },
+    {
+        q:"I was writting an email when the light went ___ .",
+        options:["down", "up", "out", "off"],
+        answer: 3
+    },
+    {
+        q:"Yesterday I was late for school because I had to go ___ home to get my maths book.",
+        options:["back", "on", "away", "over"],
+        answer: 0
+    },
+    {
+        q:"There's a party going ___ at my neighbours' house.",
+        options:["away", "on", "out", "after"],
+        answer: 1
+    },
+    {
+        q:"Taxes never seem to go ___ . The government exacts more and more from us.",
+        options:["away", "up", "down", "back"],
+        answer: 2
+    },
+    {
+        q:"We can't go ___ with the project without them. They are a crucial part.",
+        options:["up", "out", "down", "ahead"],
+        answer: 3
+    },
+    {
+        q:"The local festival will go ___ as planned next month.",
+        options:["ahead", "away", "over", "down"],
+        answer: 0
+    },
+    {
+        q:"When the headmaster entered, the teacher went ___ explaining the new topic.",
+        options:["away", "on", "out", "after"],
+        answer: 1
+    },
+    {
+        q:"Every morning my mum goes ___ my room to say 'Hello'.",
+        options:["on", "away", "into", "out"],
+        answer: 2
+    },
+    {
+        q:"Life expectancy has gone ___ from 70 to 74 recently.",
+        options:["on", "over", "off", "up"],
+        answer: 3
+    }
+]
